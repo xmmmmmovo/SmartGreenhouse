8db337d97f06291a6f74a522545a828522611135
xmmmmmovo <13256172360@163.com>
2020-09-09 10:40:51 +0800

    2020/09/09 添加RFID MQTT协议

--- hardware/RFID_tasks.py
+++ hardware/RFID_tasks.py
@@ -4,10 +4,12 @@ import time
 import threading
 from loguru import logger
 import paho.mqtt.client as mqtt
+import requests
+import os
 
 reader = SimpleMFRC522()
 
-sensor_data_topic = 'rfid_log'
+rfid_topic = 'rfid_log'
 mqtt_client: mqtt.Client = None
 
 broker_url = '39.105.110.28'
@@ -15,6 +17,26 @@ port = 8083
 user = 'emqx'
 pwd = 'public'
 transport = 'websockets'
+
+data = {
+    'uuid': None,
+    'id': None,
+    'text': None
+}
+
+
+def load_config():
+    logger.info(f'开始读取配置信息')
+    if os.path.exists('./uuid'):
+        f = open('./uuid', 'r', encoding='utf-8')
+        data['uuid'] = f.read().strip()
+        logger.info('读取成功')
+    else:
+        logger.info('未找到，正在从云端获取')
+        data['uuid'] = requests.post('http://192.168.137.1:9000/hardware/code').json()['data']
+        f = open('./uuid', 'w', encoding='utf-8')
+        f.write(data['uuid'])
+    f.close()
 
 
 def connect_mqtt():
@@ -26,7 +48,7 @@ def connect_mqtt():
         else:
             logger.info('Failed to connect to mqtt broker!')
 
-    mqtt_client = mqtt.Client(data['uuid'] + '_sensor_client', transport='websockets')
+    mqtt_client = mqtt.Client(data['uuid'] + '_rfid_client', transport='websockets')
     mqtt_client.username_pw_set(user, pwd)
     mqtt_client.on_connect = handle_mqtt_connect
     mqtt_client.connect(broker_url, port)
@@ -46,8 +68,9 @@ def RFID_tasks():
     while True:
         try:
             id, text = reader.read()
-            print(id)
-            print(text)
+            data['id'] = id
+            data['text'] = text
+            mqtt_client.publish(rfid_topic, data)
         finally:
             GPIO.cleanup()
         time.sleep(5)
